2b7fc29778f4bfe5a4b65a7708cc055ea94cc969
nazifahamidiyati <71299762+nazifahamidiyati@users.noreply.github.com>
2020-10-05 00:22:19 +0800

    Create myscript.sh

---
+++ TXT/myscript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Nazifa Hamidiyati
+
+FILES="mylog.txt mypubkey.txt myrank.txt myscript.sh"
+SHA="SHA256SUM"
+
+rm -f $SHA $SHA.asc
+
+echo "sha256sum $FILES > $SHA"
+sha256sum $FILES > $SHA
+
+echo "sha256sum -c $SHA"
+sha256sum -c $SHA
+
+echo "gpg -o $SHA.asc -a -sb $SHA"
+gpg -o $SHA.asc -a -sb $SHA
+
+echo "gpg --verify $SHA.asc $SHA"
+gpg --verify $SHA.asc $SHA
+
+exit 0
+
+# Mon Sep 28 21:05:04 WIB 2020
